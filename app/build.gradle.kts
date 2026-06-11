89613dd10a368a7a238a59324f31c2a60e40156b
kamgi <kamgidayatov@gmail.com>
2026-06-11 15:22:34 +0300

    RCA-31: First urlConnection with threads

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
     id("kotlin-parcelize")
 }
 
@@ -44,6 +45,7 @@ android {
 
 dependencies {
     implementation(libs.coil.compose)
+    implementation(libs.kotlinx.serialization.json)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
--- app/src/main/java/com/example/recipecomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipecomposeapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.recipecomposeapp
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -13,13 +14,34 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.recipecomposeapp.data.model.CategoryDto
 import com.example.recipecomposeapp.ui.theme.RecipeComposeAppTheme
+import kotlinx.serialization.json.Json
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
 
 class MainActivity : ComponentActivity() {
     private var deepLink by mutableStateOf<Intent?>(null)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val thread = Thread {
+            val url = URL("https://recipes.androidsprint.ru/api/category")
+            val connection = url.openConnection() as HttpURLConnection
+            connection.connect()
+            val data = connection.getInputStream().bufferedReader().readText()
+            Log.i("!!!", "Body: $data" )
+            val json = Json { ignoreUnknownKeys = true }
+            val deserializeData = json.decodeFromString<List<CategoryDto>>(data)
+            Log.i("!!!", "Количество категорий: ${deserializeData.size}")
+            Log.i("!!!", "Названия категорий: ${deserializeData.joinToString { category -> category.title }}")
+            Log.i("!!!", "Выполняю запрос на потоке: ${Thread.currentThread().name}")
+        }
+        thread.start()
+
+        Log.i("!!!", "Метод onCreate() выполняется на потоке: ${Thread.currentThread().name}")
 
         intent?.data?.let { uri ->
             deepLink = intent
@@ -37,21 +59,5 @@ class MainActivity : ComponentActivity() {
             deepLink = intent
         }
         setIntent(intent)
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RecipeComposeAppTheme {
-        Greeting("Android")
     }
 }

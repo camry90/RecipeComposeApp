abef2c5a167dace5fcbfe882b63297b351b20889
kamgi <kamgidayatov@gmail.com>
2026-08-11 20:10:18 +0300

    RCA-38: small fix #1

--- app/src/main/java/com/example/recipecomposeapp/data/database/RecipesDatabase.kt
+++ app/src/main/java/com/example/recipecomposeapp/data/database/RecipesDatabase.kt
@@ -22,7 +22,7 @@ abstract class RecipesDatabase : RoomDatabase() {
                 RecipesDatabase::class.java,
                 "recipes_database"
             )
-                .fallbackToDestructiveMigration(false)
+                .fallbackToDestructiveMigration()
                 .build()
         }
     }
